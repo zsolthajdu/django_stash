eee3194c5e2b2a04b4155202d57e8869afabe940
Zsolt Hajdu <z_hajdu@yahoo.com>
2018-04-01 13:09:49 -0700

    Updated urls to be django2 compatible

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls import url, include
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from .models import Bookmark
@@ -7,12 +7,8 @@ from .models import Bookmark
 from .views import CreateView, DetailsView
 
 urlpatterns = {
-    url(r'^bookmarks/$', CreateView.as_view(), name="create"),
-    url(r'^bookmarks/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
-
-    #url(r'^users/$', UserView.as_view(), name="users"),
-    #url(r'users/(?P<pk>[0-9]+)/$',
-    #            UserDetailsView.as_view(), name="user_details"),
+    path('bookmarks/', CreateView.as_view(), name="create"),
+    path('bookmark/<int:pk>/', DetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
