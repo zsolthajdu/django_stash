c099b9b2f1036151010ca77b231d1788952be464
Zsolt Hajdu <z_hajdu@yahoo.com>
2018-08-25 19:28:42 -0700

    Removed app name from custom paths.

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -7,12 +7,11 @@ from .models import Bookmark
 from .views import CreateView, DetailsView, SearchView, TagSearchView
 
 urlpatterns = {
-    path('bookmarks/', CreateView.as_view(), name="create"),
-    path('bookmark/<int:pk>/', DetailsView.as_view(), name="details"),
-    path('bookmarks/search/<str:term>/', SearchView.as_view(), name="bookmark-search"),
-    path('bookmarks/tags/<str:term>/', TagSearchView.as_view(), name="bookmark-tags"),
+    path('', CreateView.as_view(), name="create"),
+    path('<int:pk>', DetailsView.as_view(), name="details"),
+    path('search/<str:term>/', SearchView.as_view(), name="bookmark-search"),
+    path('tags/<str:term>/', TagSearchView.as_view(), name="bookmark-tags"),
     path('api-auth/', include('rest_framework.urls')),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
-
